01521a2e2beb5bc62387872859fca5c11c73b867
Moisès Macià <mmacia@gmail.com>
2024-05-26 16:42:18 +0200

    first commit

---
+++ lib/rpg_combat.ex
@@ -0,0 +1,18 @@
+defmodule RpgCombat do
+  @moduledoc """
+  Documentation for `RpgCombat`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RpgCombat.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rpg_combat_test.exs
@@ -0,0 +1,8 @@
+defmodule RpgCombatTest do
+  use ExUnit.Case
+  doctest RpgCombat
+
+  test "greets the world" do
+    assert RpgCombat.hello() == :world
+  end
+end
